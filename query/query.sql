c988d5c2b281783531ede4712e1f859befcdd3a7
Everson Dias <eversond14s@gmail.com>
2023-08-15 16:05:11 -0300

    update

--- query/query.sql
+++ query/query.sql
@@ -1,129 +1,23 @@
--- todos os clientes que tem cartão de credito
+-- todos os produtos disponíveis
 
 SELECT 
-	CONCAT(first_name,' ',middle_name,' ',last_name) AS nome_completo,
-  phone_number AS numero_de_telefone,
-  email,
-  CONCAT(street,'-',neighborhood,'-',state,'-',city,'-') AS endereço,
-  birth_date AS data_de_nascimento
+	e.name AS corretor,
+  p.address As endereço,
+  p.phone_number As telefone,
+  p.price AS valor
 FROM 
-	client AS c, wallet AS w 
+	employee AS e, product AS p, job AS j 
 WHERE 
-  c.id = w.client_id AND w.type_card = 'CREDITO';
+  p.employee_id = e.id AND e.job_id = j.id AND j.role = 'Corretor';
 
--- todos os clientes com compras em aberto
+-- ver todos os produtos vendidos
 
 SELECT 
-	CONCAT(first_name,' ',middle_name,' ',last_name) AS nome_completo,
-  phone_number AS numero_de_telefone,
-  email,
-  CONCAT(street,'-',neighborhood,'-',state,'-',city,'-') AS endereço,
-  birth_date AS data_de_nascimento
+	(SELECT e.name FROM product as p, employee as e WHERE p.employee_id = e.id AND p.id = sh.product_id) AS capitador,
+  e.name AS vendedor,
+  p.price AS valor,
+  sh.commission AS comissão,
 FROM 
-	client AS c,
-  orders AS o,
-  payment AS p
+	employee AS e, product AS p, sales_history AS sh 
 WHERE 
-  c.id = o.client_id AND o.id = p.orders_id AND p.status = 'EM PROGRESSO';
-
--- todos os vendedores com venda acime de 10 produtos em ordem decrescente
-
-SELECT 
-	fantasy_name AS nome,
-  phone_number AS numero_de_telefone,
-  email,
-  (
-		SELECT
-			SUM(quantity)
-		FROM
-			sales_history
-		WHERE
-			sales_history.seller_id = id
-	) AS quantidade_de_vendas
-FROM 
-	seller AS s HAVING quantidade_de_vendas > 10;
-
--- os 5 produto mais vendido
-
-SELECT 
-	name AS nome_do_produto,
-  category AS categoria,
-  description AS descrição,
-  assessment AS nota,
-  price AS preço,
-  (
-		SELECT
-			SUM(quantity)
-		FROM
-			sales_history
-		WHERE
-			sales_history.product_id = id
-	) AS quantidade_de_vendas
-FROM 
-	product ORDER BY quantidade_de_vendas DESC LIMIT 5;
-
--- os produtos com menos de 10 vendas
-
-SELECT 
-	name AS nome_do_produto,
-  category AS categoria,
-  description AS descrição,
-  assessment AS nota,
-  price AS preço,
-  (
-		SELECT
-			SUM(quantity)
-		FROM
-			sales_history
-		WHERE
-			sales_history.product_id = id
-	) AS quantidade_de_vendas
-FROM 
-	product HAVING quantidade_de_vendas < 10;
-
--- faturamento dos vendedores no mês de agosto
-
-SELECT 
-	fantasy_name AS nome,
-  phone_number AS numero_de_telefone,
-  email,
-  SUM((ps.quantity * p.price)) AS faturamento
-FROM 
-	seller AS s,
-  sales_history AS ps,
-  product AS p
-WHERE s.id = ps.seller_id AND ps.product_id = p.id AND ps.sale_date LIKE '2023-08-%'
-GROUP BY s.id;
-
--- Resumo das vendas
-
-SELECT 
-	fantasy_name AS nome,
-  phone_number AS numero_de_telefone,
-  email,
-  ps.sale_date AS data_da_venda,
-  ps.quantity AS quantidade_vendida,
-  p.price AS preço_do_produto,
-  p.name AS nome_do_produto,
-  p.category AS categoria_do_produto,
-  (ps.quantity * p.price) AS valor_total
-FROM 
-	seller AS s,
-  sales_history AS ps,
-  product AS p
-WHERE s.id = ps.seller_id AND ps.product_id = p.id;
-
--- quais produtos foram estocados no mês de agosto
-
-SELECT 
-	p.name AS nome,
-  pp.quantity AS quantidade,
-  pp.date_provide AS data_da_compra,
-  s.corporate_name AS nome_social,
-  s.phone_number AS telefone
-FROM 
-	product AS p,
-  product_provided AS  pp,
-  supplier AS s
-WHERE
-	p.id = pp.product_id AND pp.supplier_id = s.id AND pp.date_provide LIKE '2023-08-%';
+  sh.employee_id = e.id AND sh.product_id = p.id;
--- schema.sql
+++ schema.sql
@@ -1,129 +1,48 @@
-DROP DATABASE IF EXISTS ecommerce;
-CREATE DATABASE ecommerce;
-USE ecommerce;
+DROP DATABASE IF EXISTS db_manager;
+CREATE DATABASE db_manager;
 
-CREATE TABLE client (
-  id INT PRIMARY KEY AUTO_INCREMENT,
-  first_name VARCHAR(20) NOT NULL,
-  last_name VARCHAR(20) NOT NULL,
-  middle_name VARCHAR(20) NOT NULL,
-  CPF CHAR(14) NOT NULL,
-  phone_number CHAR(15) NOT NULL,
-  email VARCHAR(50) NOT NULL,
-  birth_date DATE NOT NULL,
-  street VARCHAR(30) NOT NULL,
-  neighborhood VARCHAR(30) NOT NULL,
-  zip_code CHAR(9) NOT NULL,
-  state VARCHAR(20) NOT NULL,
-  city VARCHAR(20) NOT NULL,
-  complement VARCHAR(50),
-  CONSTRAINT client_unique_CPF UNIQUE (CPF)
+USE db_manager;
+
+CREATE TABLE job (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  role VARCHAR(50) NOT NULL,
+  contract_type VARCHAR(15) NOT NULL
 );
 
-CREATE TABLE wallet (
-  client_id INT,
-  type_card ENUM('CREDITO', 'DEBITO') NOT NULL,
-  card_number CHAR(16) NOT NULL,
-  expiration_date CHAR(5) NOT NULL,
-  secret_number CHAR(3) NOT NULL,
-  CONSTRAINT wallet_fk_client_id FOREIGN KEY (client_id) REFERENCES client (id)
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) NOT NULL UNIQUE,
+  job_id INT NOT NULL,
+  address VARCHAR(255) NOT NULL,
+  phone_number CHAR(15) NOT NULL,
+  salary DECIMAL(7,2),
+  admission_date DATETIME DEFAULT CURRENT_TIMESTAMP,
+  CONSTRAINT employee_fk_job_id FOREIGN KEY (job_id) REFERENCES job(id)
+);
+
+CREATE TABLE credential (
+  employee_id INT NOT NULL PRIMARY KEY,
+  email VARCHAR(100) NOT NULL,
+  password VARCHAR(100) NOT NULL,
+  CONSTRAINT credential_fk_employee_id FOREIGN KEY (employee_id) REFERENCES employee(id) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 CREATE TABLE product (
-  id INT PRIMARY KEY AUTO_INCREMENT,
-  name VARCHAR(50) NOT NULL,
-  category VARCHAR(20) NOT NULL,
-  description VARCHAR(200) NOT NULL,
-  assessment float DEFAULT (0),
-  price DECIMAL(10,2) NOT NULL
-);
-
-CREATE TABLE orders (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-  status ENUM('EM PROCESSO', 'EM TRANSPORTE', 'FINALIZADO'),
-  client_id INT NOT NULL,
-  description VARCHAR(200) NOT NULL,
-  freight DECIMAL(5,2) DEFAULT(10.00),
-  CONSTRAINT orders_fk_client_id FOREIGN KEY (client_id) REFERENCES client (id)
-);
-
-CREATE TABLE payment (
-  orders_id INT PRIMARY KEY,
-  type_payment ENUM('BOLETO', 'CREDITO', 'DEBITO', 'PIX') DEFAULT ('CREDITO'),
-  status ENUM('FALHA', 'SUCESSO', 'EM PROGRESSO') DEFAULT('EM PROGRESSO'),
-  CONSTRAINT payment_fk_orders FOREIGN KEY (orders_id) REFERENCES orders (id)
-);
-
-CREATE TABLE product_orders (
-  product_id INT NOT NULL,
-  orders_id INT NOT NULL,
-  quantity INT UNSIGNED NOT NULL,
-  PRIMARY KEY (product_id, orders_id),
-  CONSTRAINT product_orders_fk_product_id FOREIGN KEY (product_id) REFERENCES product (id),
-  CONSTRAINT product_orders_fk_orders_id FOREIGN KEY (orders_id) REFERENCES orders (id)
-);
-
-CREATE TABLE stock (
-  id INT PRIMARY KEY AUTO_INCREMENT,
-  street VARCHAR(30) NOT NULL,
-  neighborhood VARCHAR(30) NOT NULL,
-  zip_code CHAR(9) NOT NULL,
-  state VARCHAR(20) NOT NULL,
-  city VARCHAR(20) NOT NULL,
-  complement VARCHAR(50)
-);
-
-CREATE TABLE in_stock (
-  product_id INT,
-  stock_id INT,
-  quantity INT UNSIGNED,
-  PRIMARY KEY (product_id, stock_id),
-  CONSTRAINT in_stock_fk_stock_id FOREIGN KEY (stock_id) REFERENCES stock (id),
-  CONSTRAINT in_stock_fk_product_id FOREIGN KEY (product_id) REFERENCES product (id)
-);
-
-CREATE TABLE supplier (
-  id INT PRIMARY KEY AUTO_INCREMENT,
-  corporate_name VARCHAR(30) NOT NULL,
-  CNPJ CHAR(18) NOT NULL,
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  employee_id INT,
+  address VARCHAR(255) NOT NULL,
   phone_number CHAR(15) NOT NULL,
-  CONSTRAINT supplier_unique_CNPJ UNIQUE (CNPJ)
-);
-
-CREATE TABLE product_provided (
-  product_id INT,
-  supplier_id INT,
-  quantity INT NOT NULL,
-  date_provide DATETIME DEFAULT CURRENT_TIMESTAMP
-);
-
-CREATE TABLE seller (
-  id INT PRIMARY KEY AUTO_INCREMENT,
-  corporate_name VARCHAR(30) NOT NULL,
-  email VARCHAR(50) NOT NULL,
-  phone_number CHAR(15) NOT NULL,
-  fantasy_name VARCHAR(20) NOT NULL,
-  CNPJ CHAR(18) NOT NULL,
-  CONSTRAINT seller_unique_CNPJ UNIQUE (CNPJ)
-);
-
-CREATE TABLE bank_account (
-  seller_id INT PRIMARY KEY,
-  bank VARCHAR(20) NOT NULL,
-  agency VARCHAR(20) NOT NULL,
-  account VARCHAR(20) NOT NULL,
-  type_account ENUM('POUPANÇA', 'CORRENTE'),
-  CPF CHAR(14) NOT NULL,
-  CONSTRAINT bank_account_unique_CPF UNIQUE (CPF),
-  CONSTRAINT bank_account_fk_seller_id FOREIGN KEY (seller_id) REFERENCES seller (id)
+  available BOOLEAN DEFAULT TRUE,
+  price DECIMAL(10,2) NOT NULL,
+  CONSTRAINT product_fk_employee_id FOREIGN KEY (employee_id) REFERENCES employee(id) ON UPDATE CASCADE ON DELETE SET NULL
 );
 
 CREATE TABLE sales_history (
-  seller_id INT,
+  employee_id INT,
   product_id INT,
-  quantity INT UNSIGNED NOT NULL,
-  sale_date DATETIME DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (seller_id, product_id),
-  CONSTRAINT sales_history_fk_seller_id FOREIGN KEY (seller_id) REFERENCES seller (id),
-  CONSTRAINT sales_history_fk_product_id FOREIGN KEY (product_id) REFERENCES product (id)
+  date_sales DATETIME DEFAULT CURRENT_TIMESTAMP,
+  commission DECIMAL(4,2) NOT NULL,
+  PRIMARY KEY (employee_id,product_id),
+  CONSTRAINT sales_history_fk_employee_id FOREIGN KEY (employee_id) REFERENCES employee(id),
+  CONSTRAINT sales_history_fk_product_id FOREIGN KEY (product_id) REFERENCES product(id)
 );
